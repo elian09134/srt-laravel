ae2e12ed651d37f0176921c71ec3995c93eaff9c
unknown <elianafriliana9@gmail.com>
2026-01-08 08:37:02 +0700

    feat: block search engine indexing with robots.txt and noindex meta tags

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=1280, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Admin Panel') - TERANG</title>
     
