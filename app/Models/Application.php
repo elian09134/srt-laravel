0a437e6ac07288598f1238623dde0fdcb01e3cc3
unknown <elianafriliana9@gmail.com>
2026-01-14 09:11:38 +0700

    add download cv button

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -6,28 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    protected $fillable = [
-      'user_id',
-      'job_id',
-      'status',
-      'join_date',
-      'cover_letter',
-      'applicant_name',
-      'applicant_email',
-      'applicant_phone',
-      'applicant_last_position',
-      'applicant_last_education',
-    ];
+  protected $fillable = [
+    'user_id',
+    'job_id',
+    'status',
+    'join_date',
+    'cover_letter',
+    'applicant_name',
+    'applicant_email',
+    'applicant_phone',
+    'applicant_last_position',
+    'applicant_last_education',
+    'snapshot_data',
+  ];
   public function user()
-{
+  {
     return $this->belongsTo(User::class);
-}
+  }
 
-public function job()
-{
+  public function job()
+  {
     return $this->belongsTo(Job::class);
-}
-    
+  }
+
   public function statusHistories()
   {
     return $this->hasMany(ApplicationStatusHistory::class)->orderBy('created_at', 'desc');
